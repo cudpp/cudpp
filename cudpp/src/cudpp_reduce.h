ce7612de8548eb3548002516619d9812c02176ca
Andrew Davidson <aaldavidson.ucdavis@gmail.com>
2010-02-12 00:14:12 +0000

    tuneReduce added

--- cudpp/src/cudpp_reduce.h
+++ cudpp/src/cudpp_reduce.h
@@ -4,30 +4,30 @@
 // $Revision$
 // $Date$
 // ------------------------------------------------------------- 
-// This source code is distributed under the terms of license.txt 
+// This source code is distributed under the terms of license.txt
 // in the root directory of this source distribution.
 // ------------------------------------------------------------- 
-
-/**
-* @file
-* cudpp_reduce.h
-*
-* @brief Reduce functionality header file - contains CUDPP interface (not public)
-*/
-
 #ifndef _CUDPP_REDUCE_H_
 #define _CUDPP_REDUCE_H_
 
 class CUDPPReducePlan;
 
+extern "C"
+void allocReduceStorage(CUDPPReducePlan* plan);
 
-void allocReduceStorage(CUDPPReducePlan *plan);
+extern "C"
+void freeReduceStorage(CUDPPReducePlan* plan);
 
-void freeReduceStorage(CUDPPReducePlan *plan);
-
-void cudppReduceDispatch(void                *d_out, 
-                         const void          *d_in, 
-                         size_t              numElements,
+extern "C"
+void cudppReduceDispatch(void               *d_odata, 
+                         const void         *d_idata, 
+                         size_t             numElements, 
                          const CUDPPReducePlan *plan);
 
-#endif // _CUDPP_REDUCE_H_
+extern "C"
+void tuneReduce(CUDPPReducePlan *plan, CUDPPTuneConfig *tuneConfig);
+
+#endif
+
+
+ // __RADIXSORT_H__
