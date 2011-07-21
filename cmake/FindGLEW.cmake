cd49a69ab7d8d2b805e26cbc58b49a40a57679d6
Mark Harris <harrism@gmail.com>
2011-07-21 05:19:11 +0000

    Minor cmake fixes for OS X / linux

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -17,31 +17,30 @@ IF (WIN32)
 		${GLEW_ROOT_DIR}/lib
 		DOC "The GLEW library")
 ELSE (WIN32)
-        FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+  FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
 		/usr/include
 		/usr/local/include
 		/sw/include
 		/opt/local/include
-        ${GLEW_ROOT_DIR}/include
+    ${GLEW_ROOT_DIR}/include
 		DOC "The directory where GL/glew.h resides")
 	FIND_LIBRARY( GLEW_LIBRARY
 		NAMES GLEW libGLEW
 		PATHS
 		/usr/lib64
 		/usr/lib
-        /usr/local/lib64
+    /usr/local/lib64
 		/usr/local/lib
 		/sw/lib
 		/opt/local/lib
         ${GLEW_ROOT_DIR}/lib
 		DOC "The GLEW library")
-SET(GLEW_LIBRARY "/usr/lib/libGLEW.so")
 ENDIF (WIN32)
 
-IF (GLEW_INCLUDE_PATH)
+IF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
   SET( FOUND_GLEW 1)
-ELSE (GLEW_INCLUDE_PATH)
+ELSE (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
   SET( FOUND_GLEW 0)
-ENDIF (GLEW_INCLUDE_PATH)
+ENDIF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
 
 MARK_AS_ADVANCED( FOUND_GLEW )
