cudpp/cudpp

commit 19e99b7a3714d6fc730088cde1e98d248751bb16

Fixed one warning and made some doxygen changes. Need a midas account so I can run doxygen and check for errors.

src/cudpp/app/stringsort_app.cu
@@ -28,6 +28,7 @@
 #include "cudpp_util.h"
 #include "cudpp_stringsort.h"
 #include "kernel/stringsort_kernel.cuh"
+#include "kernel/mergesort_kernel.cuh" //for simpleCopy
 #include "limits.h"
 
 
@@ -42,6 +43,7 @@
 * @param[in,out] pvals Addresses of string locations for tie-breaks
 * @param[out] stringVals global string value array (four characters stuffed into a uint)
 * @param[in] numElements Number of elements in the sort.
+* @param[in] stringArrayLength The size of our string array in uints (4 chars per uint)
 * @param[in] plan Configuration information for mergesort.
 **/
 void runStringSort(unsigned int *pkeys, 
@@ -238,6 +240,7 @@ extern "C"
 	* @param[in,out] values Address of string values in array of null terminated strings
 	* @param[in] stringVals Global string array
 	* @param[in] numElements Number of elements in the sort.
+	* @param[in] stringArrayLength The size of our string array in uints (4 chars per uint)
 	* @param[in] plan Configuration information for mergeSort.
 	**/
 

src/cudpp/cta/stringsort_cta.cuh
@@ -8,7 +8,7 @@
 // in the root directory of this source distribution.
 // ------------------------------------------------------------- 
 #include <cudpp_globals.h>
-#include "cudpp_mergesort.h"
+#include "cudpp_stringsort.h"
 #include <cudpp.h>
 #include <stdio.h>
 
@@ -34,27 +34,26 @@
 #define BLOCKSORT_SIZE 1024
 #define CTA_BLOCK 128
 #define DEPTH_simple 2
-#define DEPTH_multi 2
+#define DEPTH_multi 4
 #define CTASIZE_simple 256
-#define CTASIZE_multi 256
+#define CTASIZE_multi 128
 
 #define INTERSECT_A_BLOCK_SIZE_simple DEPTH_simple*CTASIZE_simple
 #define INTERSECT_B_BLOCK_SIZE_simple 2*DEPTH_simple*CTASIZE_simple
 
 #define INTERSECT_A_BLOCK_SIZE_multi DEPTH_multi*CTASIZE_multi
 #define INTERSECT_B_BLOCK_SIZE_multi 2*DEPTH_multi*CTASIZE_multi
-
 typedef unsigned int uint;
 
 /** @brief Breaks ties in keys (first four characters) returns true if cmpVal > myVal false otherwise
  * @param[in] myLoc, cmpLoc Location of the two inputs
  * @param[in] myBound, cmpBound Local memory bounds for the two addresses
- * @param[in] myAdd, cmpAdd Address into global memory of the two values add[myLoc], add[cmpLoc]
+ * @param[in] myAdd, Address into global memory of our current value
+ * @param[in] cmpAdd, Address into global memory of the value we are comparing against
  * @param[in] stringLoc Global memory array (input string)
  * @param[in] stringSize Size of our input string
+ * @return Returns 1 if cmpVal > myVal 0 otherwise
  **/
-
-
 __device__ int tie_break_simp(unsigned int myLoc, unsigned int cmpLoc, unsigned int myBound, unsigned int cmpBound, unsigned int myAdd, unsigned int cmpAdd, unsigned int* stringLoc, unsigned int stringSize)
 {
 	
@@ -86,12 +85,15 @@ __device__ int tie_break_simp(unsigned int myLoc, unsigned int cmpLoc, unsigned 
 	
 }
 /** @brief Binary search within a single block (blockSort)
- * @param[in/out] cmpValue Value being considered from other partition
+ * @param[in,out] cmpValue Value being considered from other partition
  * @param[in] tmpVal My Value
- * @param[in] in, addressPad, stringVals in = keys, addressPad = values, stringVals are used to break ties
- * @param[in/out] j, The index we are considering
- * @param[in] sizeRemain, Size of our block (if it's smaller than blockSize)
- * @param[in] stringSize, Size of our global string array (for tie breaks)
+ * @param[in] in, input keys
+ * @param[in] addressPad addresses of string locations in case of tie breaks
+ * @param[in] stringVals global string array used to break ties
+ * @param[in,out] j The index we are considering
+ * @param[in] bump The offset we update by
+ * @param[in] sizeRemain Size of our block (if it's smaller than blockSize)
+ * @param[in] stringSize Size of our global string array (for tie breaks)
  **/
 template<class T, int depth>
 __device__ void bin_search_block_string(T &cmpValue, T tmpVal, T* in, T* addressPad, T* stringVals, int & j, int bump, int sizeRemain, unsigned int stringSize)
@@ -119,19 +121,18 @@ __device__ void bin_search_block_string(T &cmpValue, T tmpVal, T* in, T* address
 }
 
 /** @brief Linear search within a single block (blockSort)
- * @param[in/out] cmpValue Value being considered from other partition
- * @param[in/out] tmpVal Temporary register which is used initially to compare our value, and then to store the final address
+ * @param[in,out] cmpValue Value being considered from other partition
+ * @param[in,out] tmpVal Temporary register which is used initially to compare our value, and then to store the final address
  *                        after our search
  * @param[in] in, addressPad, stringVals in = keys, addressPad = values, stringVals = global string array for tie breaks
- * @param[in] j, index in B partition we are considering
- * @param[in] offset, Since this is register packed, offset is the ith iteration of linear search
- * @param[in] last, The end of partition B we are allowed to look upto
- * @param[in] startAddress, The beginning of our partition
- * @param[in] stringSize, Size of our global string array
+ * @param[in] j index in B partition we are considering
+ * @param[in] offset Since this is register packed, offset is the ith iteration of linear search
+ * @param[in] last The end of partition B we are allowed to look upto
+ * @param[in] startAddress The beginning of our partition 
+ * @param[in] stringSize Size of our global string array
  **/
 template<class T, int depth>
-__device__ void lin_search_block_string(T &cmpValue, T &tmpVal, T* in, T* addressPad, T* stringVals, int &j, int offset, int last, int startAddress, int addPart, int sizeRemain, int stringSize)
-{			
+__device__ void lin_search_block_string(T &cmpValue, T &tmpVal, T* in, T* addressPad, T* stringVals, int &j, int offset, int last, int startAddress, int stringSize){			
 	
 	
 	while (cmpValue < tmpVal && j < last)		
@@ -190,9 +191,11 @@ __device__ void lin_search_block_string(T &cmpValue, T &tmpVal, T* in, T* addres
 }
 
 /** @brief For blockSort. Compares two values and decides to swap if A1 > A2
- * @param[in/out] A1, A2 Two values being compared
- * @param[in] index1, index Local addresses of values
- * @param[in/out] scratch, Scratch memory storing the addresses
+ * @param[in,out] A1 First value being compared
+ * @param[in,out] A2 Second value being compared
+ * @param[in] index1, Local address of A1
+ * @param[in] index2, Local address of A2 
+ * @param[in,out] scratch, Scratch memory storing the addresses
  * @param[in] stringVals, String Values for tie breaks
  * @param[in] size, size of our array
  *                        
@@ -290,10 +293,11 @@ void  binSearch_frag_mult(T* keyArraySmem, T* valueArraySmem, int offset, int &m
  * @param[in] myKey, myAddress Keys and address from our array
  * @param[in] index Current index we are considering in our B array
  * @param[in] BKeys, BValues Keys and Addresses for array B
- * @param[in, out] stringValues, A_Keys, A_values, A_keys_out, A_values_out Global arrays for our strings, keys, values
+ * @param[in, out] stringValues, A_keys, A_values, A_keys_out, A_values_out Global arrays for our strings, keys, values
  * @param[in] myStartIdxA, myStartIdxB, myStartIdxC Beginning indices for our partitions
  * @param[in] localMinB, localMaxB The minimum and maximum values in our B partition
- * @param[in] aCont, bCount, totalSize, mySizeA, mySizeB, stringSize Address bounds and calculation helpers
+ * @param[in] aCont, bCont, totalSize, mySizeA, mySizeB, stringSize Address bounds and calculation helpers
+ * @param[in] i, The index of the local element we are merging
  * @param[in] stepNum Debug helper
  * @param[in] placed Whether value has been placed yet or not
 **/
@@ -392,7 +396,6 @@ void lin_merge_simple(T& cmpValue, T myKey, T myAddress, int& index, T* BKeys, T
 		
 }
 
-//TODO: Make both linearMerge work for both multi and simple merge
 template<class T, int depth>
 __device__
 void linearStringMerge(T* BKeys, T* BValues, T myKey, T myAddress, bool &placed, int &index,  T &cmpValue, T* A_keys, T* A_values, T* A_keys_out, 

src/cudpp/kernel/stringsort_kernel.cuh
@@ -13,6 +13,7 @@
 #include "sharedmem.h"
 #include "cta/stringsort_cta.cuh"
 
+
 /**
  * @file
  * stringsort_kernel.cu
@@ -29,30 +30,13 @@
  */
 
 
-/** @brief Copies unused portions of arrays in our ping-pong strategy
- * @param[in] A_keys_dev, A_vals_dev The keys and values we will be copying
- * @param[out] A_keys_out_dev, A_vals_out_dev The keys and values array we will copy to
- * @param[in] offset, The offset we are starting to copy from 
- * @oaran[in] numElementsToCopy, The number of elements we copy starting from the offset
-**/
-template <class T>
-__global__
-void simpleCopy(T* A_keys_dev, unsigned int* A_vals_dev, T* A_keys_out_dev, unsigned int* A_vals_out_dev, int offset, int numElementsToCopy)
-{
-    int myId = blockIdx.x*blockDim.x + threadIdx.x;
-    if(myId >= numElementsToCopy)
-        return;
-    A_keys_out_dev[offset+myId] = A_keys_dev[offset+myId];
-    A_vals_out_dev[offset+myId] = A_vals_dev[offset+myId];
-
-}
 
 /** @brief Does an initial blockSort based on the size of our partition (limited by shared memory size)
- * @param[in/out] A_keys, A_address This sort is in-place. A_keys and A_address store the key (first four characters) and addresses of our strings
+ * @param[in/out] A_keys, A_address, This sort is in-place. A_keys and A_address store the key (first four characters) and addresses of our strings
  * @param[in] stringVals Global array of strings for tie breaks
- * @param[in] blockSize, The size of each block
- * @param[in] totalSize, The totalSize of the array we are sorting
- * @param[in] stringSize, The size of our string array (stringVals)
+ * @param[in] blockSize size of each block
+ * @param[in] totalSize The total size of the array we are sorting
+ * @param[in] stringSize The size of our string array (stringVals)
  **/
 template<class T, int depth>
 __global__
@@ -234,13 +218,13 @@ void blockWiseStringSort(T *A_keys, T* A_address, T* stringVals, int blockSize, 
 		__syncthreads();	
 		__threadfence();
         Aval[0] = j+startAddress;			
-		lin_search_block_string<T, depth>(cmpValue,  Aval[1], in, addressPad, stringVals, j, 1, last, startAddress, 0, totalSize-bid*blockSize, stringSize);				
-		lin_search_block_string<T, depth>(cmpValue,  Aval[2], in, addressPad, stringVals, j, 2, last, startAddress, 0, totalSize-bid*blockSize, stringSize);		
-		lin_search_block_string<T, depth>(cmpValue,  Aval[3], in, addressPad, stringVals, j, 3, last, startAddress, 0, totalSize-bid*blockSize, stringSize);		
-		lin_search_block_string<T, depth>(cmpValue,  Aval[4], in, addressPad, stringVals, j, 4, last, startAddress, 0, totalSize-bid*blockSize, stringSize);		
-		lin_search_block_string<T, depth>(cmpValue,  Aval[5], in, addressPad, stringVals, j, 5, last, startAddress, 0, totalSize-bid*blockSize, stringSize);		
-		lin_search_block_string<T, depth>(cmpValue,  Aval[6], in, addressPad, stringVals, j, 6, last, startAddress, 0, totalSize-bid*blockSize, stringSize);		
-		lin_search_block_string<T, depth>(cmpValue,  Aval[7], in, addressPad, stringVals, j, 7, last, startAddress, 0, totalSize-bid*blockSize, stringSize);
+		lin_search_block_string<T, depth>(cmpValue,  Aval[1], in, addressPad, stringVals, j, 1, last, startAddress, stringSize);				
+		lin_search_block_string<T, depth>(cmpValue,  Aval[2], in, addressPad, stringVals, j, 2, last, startAddress, stringSize);		
+		lin_search_block_string<T, depth>(cmpValue,  Aval[3], in, addressPad, stringVals, j, 3, last, startAddress, stringSize);		
+		lin_search_block_string<T, depth>(cmpValue,  Aval[4], in, addressPad, stringVals, j, 4, last, startAddress, stringSize);		
+		lin_search_block_string<T, depth>(cmpValue,  Aval[5], in, addressPad, stringVals, j, 5, last, startAddress, stringSize);		
+		lin_search_block_string<T, depth>(cmpValue,  Aval[6], in, addressPad, stringVals, j, 6, last, startAddress, stringSize);		
+		lin_search_block_string<T, depth>(cmpValue,  Aval[7], in, addressPad, stringVals, j, 7, last, startAddress, stringSize);
 		
 		__threadfence();
         __syncthreads();
@@ -655,7 +639,7 @@ void simpleStringMerge(T *A_keys, T *A_keys_out, T *A_values, T* A_values_out, T
 
 /** @brief For our multiMerge kernels we need to divide our partitions into smaller partitions. This kernel breaks up a set of partitions into splitsPP*numPartitions subpartitions.
  * @param[in] A_keys, A_address First four characters (input), and addresses of our inputs
- * @param[in] stringValues, Global string array for tie breaks
+ * @param[in] stringValues Global string array for tie breaks
  * @param[in] splitsPP, numPartitions, partitionSize Partition information for this routine (splitsPP=splits Per Partition) 
  * @param[in] partitionBeginA, partitionSizesA Partition starting points and sizes for each new subpartition in our original set in A
  * @param[in] partitionBeginB, partitionSizesB Partition starting points and sizes for each new subpartition in our original set in B
@@ -876,13 +860,13 @@ void findMultiPartitions(T *A_keys, T* A_address, T* stringValues, int splitsPP,
 }
 
 /** @brief Main merge kernel where multiple CUDA blocks cooperate to merge a partition(s)
- * @param[in] A_keys, A_address First four characters (input), and addresses of our inputs
+ * @param[in] A_keys, A_values First four characters (input), and addresses of our inputs
  * @param[out] A_keys_out, A_values_out First four characters, and addresses for our outputs(ping-pong)
- * @param[in] stringValues, Global string array for tie breaks
+ * @param[in] stringValues string array for tie breaks
  * @param[out] subPartitions, numBlocks Number of splits per partitions and number of partitions respectively
  * @param[in] partitionBeginA, partitionSizeA Where partitions begin and how large they are for Segment A
  * @param[in] partitionBeginB, partitionSizeB Where partitions begin and how large they are for Segment B
- * @param[in] entierPartitionSize The maximum length of a partition
+ * @param[in] entirePartitionSize The maximum length of a partition
  * @param[in] step Number of merge cycles done
  * @param[in] size Number of total strings being sorted
  * @param[in] stringSize Length of string array
@@ -1213,4 +1197,5 @@ void stringMergeMulti(T *A_keys, T*A_keys_out, T* A_values, T *A_values_out, T* 
 }
 
 		
-
+/** @} */ // end StringSort functions
+/** @} */ // end cudpp_kernel

apps/cudpp_testrig/cudpp_testrig.cpp
@@ -173,7 +173,7 @@ int testAllOptionsAndDatatypes(int argc,
 	    config.datatype = CUDPP_UINT;
         retval += testAllDatatypes(argc, argv, config, supportsDouble, multiRow);
 		return retval;
-    }
+    } 
     
     config.op = CUDPP_ADD;
     
@@ -403,10 +403,7 @@ int main(int argc, const char** argv)
             config.algorithm = CUDPP_SCAN;
             retval += testAllOptionsAndDatatypes(argc, argv, config, supportsDouble);        
         }
-        if (runMergeSort) {					            
-            config.algorithm = CUDPP_SORT_MERGE;
-            retval += testAllOptionsAndDatatypes(argc, argv, config, supportsDouble);
-        }
+       
         if (runSegScan) {
             config.algorithm = CUDPP_SEGMENTED_SCAN;
             retval += testAllOptionsAndDatatypes(argc, argv, config, supportsDouble);                        
@@ -426,6 +423,12 @@ int main(int argc, const char** argv)
             config.algorithm = CUDPP_SORT_RADIX;
             retval += testAllOptionsAndDatatypes(argc, argv, config, supportsDouble);
         }
+
+		if (runMergeSort) {					            
+            config.algorithm = CUDPP_SORT_MERGE;
+            retval += testAllOptionsAndDatatypes(argc, argv, config, supportsDouble);
+        }
+
         if(runStringSort) {
 			
             config.algorithm = CUDPP_SORT_STRING;

apps/cudpp_testrig/test_stringsort.cpp
@@ -89,7 +89,7 @@ int stringSortTest(CUDPPHandle theCudpp, CUDPPConfiguration config, size_t *test
     }
     stringVals = (unsigned int*) malloc(sizeof(unsigned int)*stringSize);
     unsigned int index = 0;
-	printf("%u elements and %d characters\n", numElements, 4*stringSize);
+	printf("%lu elements and %d characters\n", numElements, 4*stringSize);
 	unsigned int temp = 0;
 	char c1, c2, c3, c4;
     for(unsigned int i = 0; i < numElements; ++i)

src/cudpp/cta/mergesort_cta.cuh
@@ -45,7 +45,14 @@
 #define INTERSECT_B_BLOCK_SIZE_multi 2*DEPTH_multi*CTASIZE_multi
 typedef unsigned int uint;
 
-//does a portion of binary search
+/** @brief Binary search within a single block (blockSort)
+ * @param[in,out] cmpValue Value being considered from other partition
+ * @param[in] tmpVal My Value
+ * @param[in] in input keys
+ * @param[in,out] j The index we are considering
+ * @param[in] bump The offset we update by
+ * @param[in] addPart Tie break (left partition vs right partition) 
+ **/
 template<class T, int depth>
 __device__ void bin_search_block(T &cmpValue, T tmpVal, T* in, unsigned int & j, unsigned int bump, unsigned int addPart)
 {
@@ -56,10 +63,18 @@ __device__ void bin_search_block(T &cmpValue, T tmpVal, T* in, unsigned int & j,
     __syncthreads();
 
 }
-//linear search to find next index
-//addPart is needed to determine if you are the "right" block or "left" block
-
-//lin_search_block<T, depth>(cmpValue, myKey[1], myAddress[1], scratchPad, addressPad, j, 1, last, startAddress, addPart);				
+/** @brief Linear search within a single block (blockSort)
+ * @param[in,out] cmpValue Value being considered from other partition
+ * @param[in,out] cmpValue Value in our partition
+ * @param[in,out] tmpVal Temporary register which is used to store the final address after our search
+ * @param[in] in, addressPad, in = keys and addressPad = values
+ * @param[in] j index in B partition we are considering
+ * @param[in] offset Since this is register packed, offset is the ith iteration of linear search
+ * @param[in] last The end of partition B we are allowed to look upto
+ * @param[in] startAddress The beginning of our partition 
+ * @param[in] addPart Tie break (left partition vs right partition) 
+ **/
+			
 template<class T, int depth>
 __device__ void lin_search_block(T &cmpValue, T mVal, unsigned int &tmpVal, T* in, unsigned int* addressPad, unsigned int &j, 
 								 unsigned int offset, unsigned int last, unsigned int startAddress, unsigned int addPart)
@@ -76,7 +91,12 @@ __device__ void lin_search_block(T &cmpValue, T mVal, unsigned int &tmpVal, T* i
     tmpVal = j+startAddress+offset;
 }
 
-//Key-Value compare and swap
+/** @brief For blockSort. Compares two values and decides to swap if A1 > A2
+ * @param[in,out] A1 First value being compared
+ * @param[in,out] A2 Second value being compared
+ * @param[in] ref1, Local address of A1
+ * @param[in] ref2, Local address of A2                        
+ **/
 template<class T>
 __device__ void compareSwapVal(T &A1, T &A2, unsigned int& ref1, unsigned int& ref2)
 {
@@ -86,9 +106,9 @@ __device__ void compareSwapVal(T &A1, T &A2, unsigned int& ref1, unsigned int& r
         A1 = A2;
         A2 = tmp;
 
-        tmp = ref1;
+        unsigned int tmp2 = ref1;
         ref1 = ref2;
-        ref2 = tmp;
+        ref2 = tmp2;
     }   
 }
 
@@ -135,8 +155,26 @@ inline void binSearch_whole_higher(T* BKeys, int &index, T myKey)
 	binSearch_fragment_higher<T> (BKeys, 1,   index, myKey);							
 }
 
-#define DVAL1  1043863958
-#define DVAL2  1043863958
+
+/** @brief Performs a linear search in our shared memory (done after binary search).
+* It merges the partition on the left side with the associated partition on the right side
+* @param[in] searchArray Array of keys
+* @param[in] myKey Current key being considered
+* @param[in] myVal Associated value of key
+* @param[in,out] index Index in local B partition we are comparing with
+* @param[out] saveGlobalArray Array of Keys after merge is complete
+* @param[out] saveValueArray Array of values after merge is complete
+* @param[in] myStartIdxC Global starting index of both partitions being considered
+* @param[in] nextMaxB Minimum value in the partition NEXT to the one we are comparing against
+* @param[in] localAPartSize Size of the partition we are considering
+* @param[in] localBPartSize Size of the partition we are comparing against
+* @param[in] localMaxB Largest element in THIS partition we are comparing against
+* @param[in] localMinB Smallest element in THIS partition we are comparing against
+* @param[in] aIndex The first global index our block is considering (thread 0 key 0)
+* @param[in] bIndex The first global index our block is comparing against (value 0 in shared memory)
+* @param[in] offset Count of key this thread is considering (between 1 and depth)
+ 
+**/
 
 template<class T, int depth>
 __device__
@@ -161,6 +199,23 @@ inline void linearMerge_lower(T* searchArray, T myKey, unsigned int myVal, int &
 				
 }
 
+/** @brief Performs a linear search in our shared memory (done after binary search).
+* It merges the partition on the right side with the associated partition on the left side
+* @param[in] searchArray Array of keys
+* @param[in] myKey Current key being considered
+* @param[in] myVal Associated value of key
+* @param[in,out] index Index in local B partition we are comparing with
+* @param[out] saveGlobalArray Array of Keys after merge is complete
+* @param[out] saveValueArray Array of values after merge is complete
+* @param[in] myStartIdxC Global starting index of both partitions being considered
+* @param[in] localMinB Smallest element in THIS partition we are comparing against
+* @param[in] nextMaxB Minimum value in the partition NEXT to the one we are comparing against
+* @param[in] aIndex The first global index our block is considering (thread 0 key 0)
+* @param[in] bIndex The first global index our block is comparing against (value 0 in shared memory)
+* @param[in] offset Count of key this thread is considering (between 1 and depth)
+* @param[in] localAPartSize Size of the partition we are considering
+* @param[in] localBPartSize Size of the partition we are comparing against
+**/
 template<class T, int depth>
 __device__
 inline void linearMerge_higher(T* searchArray, T myKey, unsigned int myVal, int &index, T* saveGlobalArray, unsigned int* saveValueArray, int myStartIdxC, 

src/cudpp/cudpp.cpp
@@ -442,8 +442,8 @@ CUDPPResult cudppMergeSort(const CUDPPHandle planHandle,
  * @todo Determine if we need to provide an "out of place" sort interface.
  * 
  * @param[in] planHandle handle to CUDPPSortPlan
- * @param[in/out] d_keys keys (first four chars of string to be sorted)
- * @param[in/out] d_values addresses where the strings reside
+ * @param[in,out] d_keys keys (first four chars of string to be sorted)
+ * @param[in,out] d_values addresses where the strings reside
  * @param[in] stringVals Original string input, series of characters each terminated by a null
  * @param[in] numElements number of elements in d_keys and d_values
  * @param[in] stringArrayLength Length in uint of the size of all strings

commit 47e107c203d8a5f3e1a290ce8099e4f3608a2543

Fixing regression from 19e99b7a3714d6fc730088cde1e98d248751bb16/r345 that causes strinsort to fail for large inputs.  Seperated back apart the constants and shared functions into their respective files.  Future refactoring still needs to be done.

src/cudpp/app/stringsort_app.cu
@@ -28,7 +28,6 @@
 #include "cudpp_util.h"
 #include "cudpp_stringsort.h"
 #include "kernel/stringsort_kernel.cuh"
-#include "kernel/mergesort_kernel.cuh" //for simpleCopy
 #include "limits.h"
 
 
@@ -92,7 +91,7 @@ void runStringSort(unsigned int *pkeys,
 		numBlocks = (numPartitions&0xFFFE);	    
 		if(count%2 == 0)
 		{ 				
-			simpleStringMerge<unsigned int, 2>
+			simpleStringMerge<unsigned int, DEPTH_multi>
 				<<<numBlocks, CTASIZE_simple, sizeof(unsigned int)*(2*INTERSECT_B_BLOCK_SIZE_simple+4)>>>(pkeys, temp_keys, 				
 				pvals, temp_vals, stringVals, partitionSize*mult, numElements, count, stringArrayLength);		
 
@@ -107,7 +106,7 @@ void runStringSort(unsigned int *pkeys,
 		}
 		else
 		{			
-			simpleStringMerge<unsigned int, 2>
+			simpleStringMerge<unsigned int, DEPTH_multi>
 				<<<numBlocks, CTASIZE_simple, sizeof(unsigned int)*(2*INTERSECT_B_BLOCK_SIZE_simple+4)>>>(temp_keys, pkeys, 				
 				temp_vals, pvals, stringVals, partitionSize*mult, numElements, count, stringArrayLength);		
 			
@@ -144,7 +143,7 @@ void runStringSort(unsigned int *pkeys,
 
 			//int lastSubPart = getLastSubPart(numBlocks, subPartitions, partitionSize, mult, numElements);
 			CUDA_SAFE_CALL(cudaThreadSynchronize());
-			stringMergeMulti<unsigned int, DEPTH_multi>
+			stringMergeMulti<unsigned int, 2>
 				<<<numBlocks*subPartitions, CTASIZE_multi, (2*INTERSECT_B_BLOCK_SIZE_multi+4)*sizeof(unsigned int)>>>(temp_keys, pkeys, temp_vals, 
 				pvals, stringVals, subPartitions, numBlocks, partitionBeginA, partitionSizeA, partitionBeginB, partitionSizeB, mult*partitionSize, count, numElements, stringArrayLength);
 			CUDA_SAFE_CALL(cudaThreadSynchronize());
@@ -165,7 +164,7 @@ void runStringSort(unsigned int *pkeys,
 				partitionBeginB, partitionSizeB, numElements, stringArrayLength);											
 			CUDA_SAFE_CALL(cudaThreadSynchronize());
 			//int lastSubPart = getLastSubPart(numBlocks, subPartitions, partitionSize, mult, numElements);
-			stringMergeMulti<unsigned int, DEPTH_multi>
+			stringMergeMulti<unsigned int, 2>
 				<<<numBlocks*subPartitions, CTASIZE_multi, (2*INTERSECT_B_BLOCK_SIZE_multi+4)*sizeof(unsigned int)>>>(pkeys, temp_keys, pvals, 
 				temp_vals, stringVals, subPartitions, numBlocks, partitionBeginA, partitionSizeA, partitionBeginB, partitionSizeB, mult*partitionSize, count, numElements, stringArrayLength);
 

src/cudpp/cta/stringsort_cta.cuh
@@ -34,9 +34,9 @@
 #define BLOCKSORT_SIZE 1024
 #define CTA_BLOCK 128
 #define DEPTH_simple 2
-#define DEPTH_multi 4
+#define DEPTH_multi 2
 #define CTASIZE_simple 256
-#define CTASIZE_multi 128
+#define CTASIZE_multi 256
 
 #define INTERSECT_A_BLOCK_SIZE_simple DEPTH_simple*CTASIZE_simple
 #define INTERSECT_B_BLOCK_SIZE_simple 2*DEPTH_simple*CTASIZE_simple

src/cudpp/kernel/stringsort_kernel.cuh
@@ -30,6 +30,24 @@
  */
 
 
+/** @brief Copies unused portions of arrays in our ping-pong strategy
+ * @param[in] A_keys_dev, A_vals_dev The keys and values we will be copying
+ * @param[out] A_keys_out_dev, A_vals_out_dev The keys and values array we will copy to
+ * @param[in] offset, The offset we are starting to copy from 
+ * @oaran[in] numElementsToCopy, The number of elements we copy starting from the offset
+**/
+template <class T>
+__global__
+void simpleCopy(T* A_keys_dev, unsigned int* A_vals_dev, T* A_keys_out_dev, unsigned int* A_vals_out_dev, int offset, int numElementsToCopy)
+{
+    int myId = blockIdx.x*blockDim.x + threadIdx.x;
+    if(myId >= numElementsToCopy)
+        return;
+    A_keys_out_dev[offset+myId] = A_keys_dev[offset+myId];
+    A_vals_out_dev[offset+myId] = A_vals_dev[offset+myId];
+
+} 
+
 
 /** @brief Does an initial blockSort based on the size of our partition (limited by shared memory size)
  * @param[in,out] A_keys, A_address This sort is in-place. A_keys and A_address store the key (first four characters) and addresses of our strings