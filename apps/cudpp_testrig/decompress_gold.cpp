ed6ca4574988222d7cf1cab4dd2a372b51a0adf0
tsgibson <tsgibson@ucdavis.edu>
2014-08-21 14:10:44 -0700

    Finished prelimiary structure of decompress_gold

--- apps/cudpp_testrig/decompress_gold.cpp
+++ apps/cudpp_testrig/decompress_gold.cpp
@@ -19,6 +19,7 @@ int computeDecompressGold(/*  Params:
                            *        - input file (uncompressed)
                            *     Out:
                            *        - output file (compressed)
+                           *        - final huffman tree object
                            */)
 {
     /*  Steps:
@@ -38,20 +39,69 @@ int computeBWT(/*  Params:
                 *           input characters, just rearranged)
                 */)
 {
-/*  Steps:
- *     - Allocated memory for all rotations
- *     - Calculate every rotation (loop 'array_len' times)
- *     - Sort rotations in lexigraphical order
- *     - Take last character from each rotation (in order) and add (in order)
- *       to output array
- */
+    /*  Steps:
+     *     - Allocated memory for all rotations
+     *     - Calculate every rotation (loop 'array_len' times)
+     *     - Sort rotations in lexigraphical order
+     *     - Take last character from each rotation (in order) and add to
+     *       output array in order
+     */
 }
 
 // Run a Move-To-Front (MTF) Transform for computeDecompressGold()
 int computeMTF(/*  Params:
                 *     In: - input data array from BWT (i_data: original input
-                            characters in random order)
+                *           characters in random order)
                 *         - length of input data array (array_len)
                 *     Out: 
-                *         - MTF transformed data (o_data, list of numbers)
+                *         - MTF transformed data (o_data: list of numbers)
                 */)
+{
+    /*  Steps:
+     *     - Generate list of characters in array and sort (loop through)
+     *     - Loop through array, this time assigning a number to each
+     *       character corresponding to that charater's position in the MTF
+     *       list. Then move that character to the front of the MTF list
+     *     - Store those numbers in order in an array
+     */
+}
+
+// Build a huffman tree for computeDecompressGold()
+int computeHuffmanTree(/*  Params:
+                        *     In: - input data array from MTF (i_data: array of
+                        *           numbers)
+                        *         - length of input data array (array_len)
+                        *     Out: 
+                        *         - huffman coded data (o_data: list of binary
+                        *           numbers)
+                        *         - huffman tree (huffman_tree: huffman tree
+                        *           object)
+                        */)
+{
+    /*  Steps:
+     *     - Loop through input array and calculate frequency of numbers.
+     *       Assign to "huffman pairs" (key(number)-value(frequency) tuple)
+     *     - Sort array by values (frequencies) from least to most
+     *     - Copy array for huffman tree
+     *     - Create new huffman tree object. Add lowest 2 items from huffman
+     *       tree array to the tree. Remove those two items from huffman tree
+     *       array. Add new item with value = sum of previous 2 item
+     *       frequencies. Sort huffman tree array.
+     *     - Repeat until only 1 item remains in huffman tree array. Add root
+     *       to huffman tree.
+     */
+
+    /*  Data required:
+     *     - HuffmanTree object (contains nodes of "huffman pairs")
+     *       Attributes:
+     *          - Array of pointers to nodes
+     *          - Pointer to root node
+     *          - (int) Number of nodes
+     *     - HuffmanNode object
+     *       Attributes:
+     *          - (enum) Type: Root, Internal, or Leaf
+     *          - Pointers to Parent, Left Child, Right Child (NULL if
+     *            attribute does not exist)
+     *          - (int) Value: Number that the node represents (frequency from
+     *            output of MTF transform, NULL if internal or root node)
+}
