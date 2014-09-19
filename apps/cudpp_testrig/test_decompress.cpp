944a90bdfbf3e35b575ad9c18939c2de2161b0ad
tsgibson <tsgibson@ucdavis.edu>
2014-09-19 16:25:49 -0700

    Fixed the random sequence generator

--- apps/cudpp_testrig/test_decompress.cpp
+++ apps/cudpp_testrig/test_decompress.cpp
@@ -13,27 +13,31 @@
  */
 
 #include <iostream>
+#include <ctime>
 #include "decompress_gold.cpp"
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
+
     int ret_val = 0;
-    int length = 100;
-    unsigned char* input;
+    int length = 44;
+    unsigned char* input = new unsigned char[length];
+    unsigned char input2[] = "The quick brown fox jumps over the lazy dog.";
 
     if (argc > 1 && argv[1] == string("rand")) {
-        if (argc > 2 && argv[2] != string("q")) length = (int)*argv[2];
         //input = new unsigned char[length + 1];
         input = new unsigned char[length];
 
-        for (int i=0; i<length; i++)
+        for (int i=0; i<length; i++) {
             input[i] = (rand() % 255) + 1;
-
-        //input[length] = (unsigned char)*"\0";
+        }
     }
-    //unsigned char input[] = "The quick brown fox jumps over the lazy dog.";
     size_t num_elements = length;
-    ret_val = computeDecompressGold(input, num_elements, !(argc-1) || ((argv[argc-1] == string("q")) ? false : true));
+    ret_val = (argc > 1 ? computeDecompressGold(input, num_elements, !(argc-1) || ((argv[argc-1] == string("q")) ? false : true)):
+                          computeDecompressGold(input2, num_elements, !(argc-1) || ((argv[argc-1] == string("q")) ? false : true)));
+
+    delete [] input;
 
     return ret_val;
 }
