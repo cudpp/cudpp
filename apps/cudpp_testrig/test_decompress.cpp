806f5c696cb2525f3d3d9c018f63cfe50394601e
tsgibson <tsgibson@ucdavis.edu>
2014-09-22 17:44:41 -0700

    Input can now come from a file specified as an argument. Error handling now works to check vailidity of file name. Planning to expand use of error handling as code is further developed. Added comments.

--- apps/cudpp_testrig/test_decompress.cpp
+++ apps/cudpp_testrig/test_decompress.cpp
@@ -13,6 +13,7 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <ctime>
 #include <string>
 #include <exception>
@@ -29,40 +30,43 @@ class myError : exception {
 
 int main(int argc, char* argv[])
 {
-    int ret_val = 0;
-    try {
-    srand(time(NULL));
+    srand(time(NULL));  // Used to generate random characters every time the code is run
 
-    int length = 44;
-    bool verbose = true;
-    unsigned char* input = new unsigned char[length+1];
+    int ret_val = 0;  // Stores the return value
+    int length = 44;  // Default input array length (based on default input string). Changes if input comes from a file
+    bool verbose = true;  // Determines whether the program prints output data or not
+    unsigned char* input = new unsigned char[length];  // Input data array. Initialized for the default input string but is reinitialized if input comes from a different source
 
-    strcpy((char*)input, ("The quick brown fox jumps over the lazy dog."));
+    try {  // Try-catch block used to handle errors with files
+	if (argc > 1) {  // If there are any command-line arguments, process them
+	    for (int i=1; i<argc; i++) {  // Loop through all the command-line arguments
+		if (argv[i] == string("q")) verbose = false;  // "Quiet mode". Disables printing of output data
+		else if (argv[i] == string("e")) throw myError("This is an error");  // Option to test try-catch functionality
+		else if (argv[i] == string("f")) throw string("This is also an error");  // Another option to test try-catch funtionality
+		else if (argv[i] == string("rand")) for (int j=0; j<length; j++) { input[j] = (rand() % 126) + 1; }  // Generates random ASCII characters as input
+		else {  // If an option exists that isn't recognized, try and use it as a file name
+                    ifstream input_file(argv[i]);  // Try and open the file
+                    if (!input_file.is_open()) throw string("Error opening file: " + string(argv[i]));  // If the file doesn't exist, throw an error
 
-    if (argc > 1) {
-        for (int i=1; i<argc; i++) {
-            if (argv[i] == string("q")) verbose = false;
-            else if (argv[i] == string("e")) throw myError("This is an error");
-            else if (argv[i] == string("f")) throw string("This is also an error");
-            else if (argv[i] == string("rand")) for (int j=0; j<length; j++) { input[j] = (rand() % 126) + 1; }
-            else if ((argv[i]) == "../data/") cout << "OK" << endl;
-        }
+                    input_file.seekg(0, ios::end);  // Move the pointer to the end of the file
+                    streamsize size = length = input_file.tellg();  // Return the position of the end of the file (size of file)
+                    input_file.seekg(0, ios::beg);  // Return the pointer to the beginning of the file so the file can be read
+
+                    input = new unsigned char[length+1];  // Initialize input array to size of file
+                    input_file.read((char*)input, size);  // Read file into input array
+		}
+	    }
+	}
+	else strcpy((char*)input, ("The quick brown fox jumps over the lazy dog."));  // Default input string (if nothing else is specified
+
+	size_t num_elements = length;
+	ret_val = computeDecompressGold(input, num_elements, verbose);  // Run the compression code in decompress_gold.cpp
     }
-
-    input[length] = '\0';
-    size_t num_elements = length;
-    ret_val = computeDecompressGold(input, num_elements, verbose);
+    catch (myError& ex) { cout << *ex.msg << endl; }  // Just some error handling...
+    catch (string err) { cout << err << endl; }
+    catch(...) { cout << "Error" << endl; }
 
     delete [] input;
-    }
-    catch (myError& ex) {
-        cout << *ex.msg << endl;
-    }
-    catch (string err) {
-        cout << err << endl;
-    }
-    catch(...) {
-        cout << "Error" << endl;
-    }
+
     return ret_val;
 }
